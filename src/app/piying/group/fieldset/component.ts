a444016fa1ed7722060e0380e47c0d1304d76113
chen yang <wszgrcy@gmail.com>
2025-08-11 20:51:00 +0800

    update

--- src/app/piying/group/fieldset/component.ts
+++ src/app/piying/group/fieldset/component.ts
@@ -1,12 +1,10 @@
 import { NgTemplateOutlet } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { PI_VIEW_FIELD_TOKEN, PiyingViewGroupBase } from '@piying/view-angular';
+import { Component } from '@angular/core';
+import { PiyingViewGroupBase } from '@piying/view-angular';
 
 @Component({
   selector: 'fieldset-group',
   templateUrl: './component.html',
   imports: [NgTemplateOutlet],
 })
-export class FieldsetFGC extends PiyingViewGroupBase {
-  field = inject(PI_VIEW_FIELD_TOKEN);
-}
+export class FieldsetFGC extends PiyingViewGroupBase {}
